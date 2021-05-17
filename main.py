b8b393c1533a15b5f9cf68434bfae6180f7fbce4
Oliver Harris <olivercwaharris@gmail.com>
2021-05-17 14:57:59 +0100

    added 5 of 7 teams

--- main.py
+++ main.py
@@ -1,2 +1,59 @@
+#git add .
+#git commit -m ""
+#git push origin main
+
+
+#with open('file_name.txt') as reader:
+    # Further file processing goes here
+
+
+
+
 import random
 import time
+
+
+
+Team_1_main=str("Team 1 UK: Steve, John, Dave, Barry, Stuart, Katy")
+Team_1_Steve=str("Steve - ATK:4 DEF:5")
+Team_1_John=str("John - ATK:7 DEF:1")
+Team_1_Dave=str("Dave - ATK:5 DEF:3")
+Team_1_Barry=str("Barry - ATK:8 DEF:2")
+Team_1_Stuart=str("Stuart - ATK:6 DEF:4")
+Team_1_Katy_GK=str("Katy GK - ATK:2 DEF:6")
+
+
+Team_2_main=str("Team 2 Spain: Mateo, Sofia, Isabella, Hugo, Lucas, Matias")
+Team_2_Mateo=str("Mateo - ATK:5 DEF:2")
+Team_2_Sofia=str("Sofia - ATK:6 DEF:3")
+Team_2_Isabella=str("Isabella - ATK:3 DEF:3")
+Team_2_Hugo=str("Hugo - ATK:7 DEF:2")
+Team_2_Lucas=str("Lucas - ATK:8 DEF:4")
+Team_2_Matias_GK=str("Matias GK - ATK:3 DEF:6")
+
+
+Team_3_main=str("Team 3 Canada: Jakson, Noah, Benjamin, Emma, Amelia, Chloe")
+Team_3_Jakson=str("Jakson - ATK:4 DEF:3")
+Team_3_Noah=str("Noah - ATK:5 DEF:3")
+Team_3_Benjamin=str("Benjamin - ATK:7 DEF:4")
+Team_3_Emma=str("Emma - ATK:6 DEF:5")
+Team_3_Amelia=str("Amelia - ATK:7 DEF:2")
+Team_3_Chloe_GK=str("Chloe GK - ATK:2 DEF:7")
+
+
+Team_4_main=str("Team 4 Russia: Adrik, Aleksi, Borya, Dimitri, Vladimir, Evgenii")
+Team_4_Adrik=str("Adrik - ATK:4 DEF:4")
+Team_4_Aleksi=str("Aleksi - ATK:6 DEF:3")
+Team_4_Borya=str("Borya - ATK:8 DEF:2")
+Team_4_Dimitri=str("Dimitri - ATK:7 DEF:5")
+Team_4_Vladimir=str("Vladimir - ATK:6 DEF:6")
+Team_4_Evgenii_GK=str("Evgenii GK - ATK:3 DEF:7")
+
+
+Team_5_min=str("Team 5 France: Abella, Alice, Ailie, Barrie, Caitlin, Farand")
+Team_5_Abella=str("Abella - ATK:4 DEF:2")
+Team_5_Alice=str("Alice - ATK:5 DEF:1")
+Team_5_Ailie=str("Ailie - ATK:7 DEF:4")
+Team_5_Barrie=str("Barrie - ATK:6 DEF:3")
+Team_5_Caitlin=str("Caitlin - ATK:4 DEF:2")
+Team_5_Farand_GK=str("Farand - ATK:1 DEF:5")
